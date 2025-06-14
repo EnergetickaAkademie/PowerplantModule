8ec6c8ede8298a0d97738d4879e2cd378924e9ba
Jakub Pelc <jakub.pelc@seznam.cz>
2025-06-14 22:40:54 +0200

    update ota library

--- ArduinoOTA/src/main.cpp
+++ ArduinoOTA/src/main.cpp
@@ -2,49 +2,19 @@
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
+#include <ota.h>
 #include "secrets.h"
 
-const char* ssid = SECRET_SSID;
-const char* password = SECRET_PASSWORD;
+#define BLINK_INTERVAL 500
 
 void setup() {
 	Serial.begin(115200);
 	Serial.println("Booting");
-	WiFi.mode(WIFI_STA);
-	WiFi.begin(ssid, password);
-	while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-		Serial.println("Connection Failed! Rebooting...");
-		delay(5000);
-		ESP.restart();
-	}
+	
+	connectWifi(SECRET_SSID, SECRET_PASSWORD);
+	
+	setupOTA();
 
-	// Port defaults to 8266
-	// ArduinoOTA.setPort(8266);
-
-	// Hostname defaults to esp8266-[ChipID]
-	// ArduinoOTA.setHostname("myesp8266");
-
-	// No authentication by default
-	// ArduinoOTA.setPassword((const char *)"123");
-
-	ArduinoOTA.onStart([]() {
-		Serial.println("Start");
-	});
-	ArduinoOTA.onEnd([]() {
-		Serial.println("\nEnd");
-	});
-	ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-		Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-	});
-	ArduinoOTA.onError([](ota_error_t error) {
-		Serial.printf("Error[%u]: ", error);
-		if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-		else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-		else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-		else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-		else if (error == OTA_END_ERROR) Serial.println("End Failed");
-	});
-	ArduinoOTA.begin();
 	Serial.println("Ready");
 	Serial.print("IP address: ");
 	Serial.println(WiFi.localIP());
@@ -54,14 +24,13 @@ void setup() {
 
 void blinkLED() {
 	digitalWrite(LED_BUILTIN, HIGH);
-	delay(500);
+	delay(BLINK_INTERVAL);
 	digitalWrite(LED_BUILTIN, LOW);
+	delay(BLINK_INTERVAL);
 }
 
 void loop() {
-	ArduinoOTA.handle();
+	handleOTA();
 
-
-	//blink the LED
 	blinkLED();
 }
